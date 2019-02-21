2750e06868dbfa0cb12a5ec80c59de00cf9e5ba7
Geoffrey Tan <geoffreyt@live.nl>
2019-02-22 00:20:58 +0100

    Update on click after promise (Change placeholder)

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -7,5 +7,12 @@ $.get("https://designers-oath-signatures.herokuapp.com/api/count", function(data
 
 // POST data to the api when the element with #sign is clicked
 sign.addEventListener("click", function() {
-    $.post("https://designers-oath-signatures.herokuapp.com/api/sign", { email: "signed with no e-mail address" });
+    $("#sign").attr("disabled", "disabled");
+
+    $.post("https://designers-oath-signatures.herokuapp.com/api/sign", { email: "signed with no e-mail address" })
+        .done(function() {
+            $.get("https://designers-oath-signatures.herokuapp.com/api/count", function(data) {
+                $("#signature-counter").html(data.count);
+            });
+        });
 });
