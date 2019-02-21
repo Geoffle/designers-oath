098548c9a4112317aad5c9b53ab3294130572fba
MohamadAlGhorani <moh.gh266@gmail.com>
2019-02-21 16:13:26 +0100

    verbeteren stijl

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,12 @@
-placeholder delete this
+var quizzButton = document.querySelector('main section:nth-of-type(2) button');
+var quizz = document.querySelector('main section:nth-of-type(2) iframe');
+var signElement = document.querySelector('main section:last-of-type p');
+var signGetal = parseInt(signElement.innerText);
+
+
+
+function quizzAan() {
+    quizz.classList.toggle('uitklappen');
+}
+
+quizzButton.addEventListener('click', quizzAan);
